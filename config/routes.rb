2d717d59be1918375b8d37ac0c0563ecb1c559f6
macnick <nick@macexperts.gr>
2019-12-20 12:23:26 +0200

    Add controller and views for posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # root to: "devise/registrations#new"
   devise_for :users
   resources :users, only: [:index, :show]
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_18_101601) do
+ActiveRecord::Schema.define(version: 2019_12_20_094351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "post_text"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
