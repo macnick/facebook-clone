9496feb2a784ac129b2f3ca8238880d38f800588
addod19 <addodaniellarbi@gmail.com>
2020-01-08 11:34:40 +0000

    Test pass now thanks to team work

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -4,23 +4,26 @@ require 'spec_helper'
 
 feature 'User signs up and logs in' do
 
+  scenario 'user can  log in using the log in form' do
+      visit 'users/sign_in'
+      fill_in 'Email', with: 'nick@test.com'
+      fill_in 'Password', with: 'foobar'
+      click_button 'Log in'
+      expect(page).to have_content('Signed in successfully.')
+      click_link 'Sign out'
+  end
+
   scenario 'user can  create an account using the signup form' do
     visit '/users/sign_up'
-    fill_in 'first_name', with: 'Nick'
-    fill_in 'last_name', with: 'Haras'
-    fill_in 'email', with: 'nick@test.com'
-    fill_in 'password', with: 'foobar'
-    fill_in 'password_confirmation', with: 'foobar'
+    fill_in 'First name', with: 'Nicks'
+    fill_in 'Last name', with: 'Harasm'
+    fill_in 'Email', with: 'nick@test1.com'
+    fill_in 'Password', with: 'foobar'
+    fill_in 'Password confirmation', with: 'foobar'
     click_button 'Sign up'
     expect(page).to have_content('Welcome! You have signed up successfully')
   end
 
-  scenario 'user can  log in using the log in form' do
-    visit 'users/sign_in'
-    fill_in 'email', with: 'nick@test.com'
-    fill_in 'password', with: 'foobar'
-    click_button 'Log in'
-    expect(page).to have_content('Signed in successfully')
-  end
+ 
 
 end
