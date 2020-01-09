7be06ef9c87d0b0e36c9ca79337904e3708bbdca
macnick <nick@macexperts.gr>
2020-01-09 12:15:43 +0200

    Fix rubocop.yml

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -7,24 +7,29 @@ def rnd_str
   (0...8).map { rand(97..122).chr }.join.capitalize
 end
 
-feature 'User signs up and logs in' do
-  scenario 'user can log in using the log in form' do
-    visit '/users/sign_in'
-    fill_in 'Email', with: 'nick@test.com'
-    fill_in 'Password', with: '123456'
-    click_button 'Log in'
-    expect(page).to have_content('Signed in successfully.')
-    click_link 'Sign Out'
-  end
+fname = rnd_str
 
+feature 'User signs up and logs in' do
+  
   scenario 'user can create an account using the signup form' do
     visit '/users/sign_up'
-    fill_in 'First name', with: rnd_str
+    fill_in 'First name', with: fname
     fill_in 'Last name', with: rnd_str
-    fill_in 'Email', with: "#{rnd_str}@test.com"
+    fill_in 'Email', with: "#{fname}@test.com"
     fill_in 'Password', with: 'foobar'
     fill_in 'Password confirmation', with: 'foobar'
     click_button 'Sign up'
     expect(page).to have_content('Welcome! You have signed up successfully')
   end
+
+  scenario 'user can log in using the log in form' do
+    visit '/users/sign_in'
+    fill_in 'Email', with: "#{fname}@test.com"
+    fill_in 'Password', with: 'foobar'
+    click_button 'Log in'
+    expect(page).to have_content('Signed in successfully.')
+    click_link 'Sign Out'
+  end
+  
+
 end
