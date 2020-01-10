704b137e560b49a3a4bfee7e5be13c089291f4fe
macnick <nick@macexperts.gr>
2020-01-10 13:41:19 +0200

    Fix the comment appear error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,20 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(comment_params)
+    @comment.save
+    redirect_to posts_path
+
+
+    # @post = current_user.posts.build(post_params)
+    # @post.save
+  
+  end
+
   def index
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:comment_text, :post_id)
+    end
 end
