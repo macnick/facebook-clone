710b6aeb96fd0d32e51c0e402e7b5c2075fa6b70
addod19 <addodaniellarbi@gmail.com>
2020-01-13 16:57:20 +0000

    added logic to like button, user cannot like more than once

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,12 @@
 class LikesController < ApplicationController
     def create
-      post = Post.find(params[:post_id])
-      @like = post.likes.build(like_params)
-
-      @like.save
+      if already_liked?
+        flash[:notice] = "You can't like more than once"
+      else
+        post = Post.find(params[:post_id])
+        @like = post.likes.build(like_params)
+        @like.save
+      end
       redirect_to posts_path
     end
 
@@ -14,5 +17,9 @@ class LikesController < ApplicationController
     private
       def like_params
         params.permit(:user_id, :post_id)
+      end
+
+      def already_liked?
+        Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
       end
 end
