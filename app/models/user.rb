83b597310ca2fd87ecfa1a1411fb9d2b09d2a4d1
macnick <nick@macexperts.gr>
2020-01-16 11:36:39 +0200

    Add Friendship model and migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,19 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :inverse_friendships, class_name:  "Friendship", foreign_key: "friend_id"
   validates :first_name, presence: true
   validates :last_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  def friends
+    friends_array = friendships.map {|f| f.friend if f.confirmed }
+    friends_array + inverse_friends.map { |f| f.user if f.confirmed }
+    friends_array.compact
+  end
 end
