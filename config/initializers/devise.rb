65d60e5bc360fd75778073d1b812e96c0cfa7f10
macnick <nick@macexperts.gr>
2020-01-29 12:53:14 +0200

    Add config.http_authenticatable = [:database] to devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -72,7 +72,7 @@ Devise.setup do |config|
   # enable it only for database authentication. The supported strategies are:
   # :database      = Support basic authentication with authentication key + password
   # config.http_authenticatable = false
-
+  config.http_authenticatable = [:database]
   # If 401 status code should be returned for AJAX requests. True by default.
   # config.http_authenticatable_on_xhr = true
 
